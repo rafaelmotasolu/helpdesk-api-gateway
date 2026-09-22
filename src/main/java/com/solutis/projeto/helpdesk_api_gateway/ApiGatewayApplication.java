fd22fa58f593e4bb38b0653b641a6f2f1fe3f9a8
rafaelmotacr <rafaelmota361@gmail.com>
2026-09-21 23:52:48 -0300

    feat: add ApiGatewayApplication class for Spring Boot application

--- src/main/java/com/solutis/projeto/helpdesk_api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/solutis/projeto/helpdesk_api_gateway/ApiGatewayApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelpdeskApiGatewayApplication {
+public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HelpdeskApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
